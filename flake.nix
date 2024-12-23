e0d291465b5993a9da6e22b96defd34621f99b0d
Vivian <vivian@0x76.dev>
2024-12-23 18:31:20 +0100

    fix build

--- flake.nix
+++ flake.nix
@@ -4,22 +4,36 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     let
-      buildInputs = pkgs: with pkgs; [ openssl tpm2-tss ];
-      nativeBuildInputs = pkgs:
-        with pkgs; [
+      buildInputs =
+        pkgs: with pkgs; [
+          openssl
+          tpm2-tss
+        ];
+      nativeBuildInputs =
+        pkgs: with pkgs; [
           llvmPackages.libclang
+          llvmPackages.clang
           llvmPackages.libcxxClang
           clang
           pkg-config
+          rustPlatform.bindgenHook
         ];
-    in flake-utils.lib.eachDefaultSystem (system:
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) stdenv lib;
-      in rec {
+      in
+      rec {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = cargoToml.package.name;
           version = cargoToml.package.version;
@@ -30,68 +44,52 @@
 
           buildInputs = buildInputs pkgs;
           nativeBuildInputs = nativeBuildInputs pkgs;
-          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-
-          preBuild = ''
-            export BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
-               $(< ${stdenv.cc}/nix-support/libc-cflags) \
-               $(< ${stdenv.cc}/nix-support/cc-cflags) \
-               $(< ${stdenv.cc}/nix-support/libcxx-cxxflags) \
-               ${
-                 lib.optionalString stdenv.cc.isClang
-                 "-idirafter ${stdenv.cc.cc}/lib/clang/${
-                   lib.getVersion stdenv.cc.cc
-                 }/include"
-               } \
-               ${
-                 lib.optionalString stdenv.cc.isGNU
-                 "-isystem ${stdenv.cc.cc}/include/c++/${
-                   lib.getVersion stdenv.cc.cc
-                 } -isystem ${stdenv.cc.cc}/include/c++/${
-                   lib.getVersion stdenv.cc.cc
-                 }/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${
-                   lib.getVersion stdenv.cc.cc
-                 }/include"
-               } \
-             "
-          '';
+          # LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
         };
 
         devShells.default = pkgs.mkShell {
-          shellHook = "${packages.default.preBuild}";
+          # shellHook = "${packages.default.preBuild}";
           buildInputs = buildInputs pkgs;
           nativeBuildInputs = nativeBuildInputs pkgs;
         };
-      }) // {
-        nixosModules = rec {
-          default = { config, lib, pkgs, ... }:
-            with lib;
-            let cfg = config.services.gnome-autounlock-keyring;
-            in {
-              options.services.gnome-autounlock-keyring = {
-                enable = mkEnableOption "gnome-autounlock.keyring";
+      }
+    )
+    // {
+      nixosModules = rec {
+        default =
+          {
+            config,
+            lib,
+            pkgs,
+            ...
+          }:
+          with lib;
+          let
+            cfg = config.services.gnome-autounlock-keyring;
+          in
+          {
+            options.services.gnome-autounlock-keyring = {
+              enable = mkEnableOption "gnome-autounlock.keyring";
 
-                target = mkOption {
-                  type = types.str;
-                  default = "graphical-session.target";
-                  example = "hyprland-session.target";
-                };
-              };
-
-              config = mkIf cfg.enable {
-                systemd.user.services.gnome-autounlock-keyring = {
-                  description = "Automatically unlock gnome keyring using TPM";
-                  wantedBy = [ cfg.target ];
-                  script = ''
-                    ${
-                      self.packages.${pkgs.system}.default
-                    }/bin/gnome-autounlock-keyring unlock
-                  '';
-                  serviceConfig.Type = "oneshot";
-                };
+              target = mkOption {
+                type = types.str;
+                default = "graphical-session.target";
+                example = "hyprland-session.target";
               };
             };
-          gnome-autounlock-keyring = default;
-        };
+
+            config = mkIf cfg.enable {
+              systemd.user.services.gnome-autounlock-keyring = {
+                description = "Automatically unlock gnome keyring using TPM";
+                wantedBy = [ cfg.target ];
+                script = ''
+                  ${self.packages.${pkgs.system}.default}/bin/gnome-autounlock-keyring unlock
+                '';
+                serviceConfig.Type = "oneshot";
+              };
+            };
+          };
+        gnome-autounlock-keyring = default;
       };
+    };
 }
